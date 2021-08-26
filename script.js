fd9acea47352dfe874908e2aa609b8062527a17b
Rahul Kumar Jhamtani <77382243+rahulkumarjhamtani@users.noreply.github.com>
2021-08-26 12:25:25 +0530

    quiz

---
+++ script.js
@@ -0,0 +1,42 @@
+var quiz = [
+    {
+        question: 'What is the number of planets in our solar system ?',
+        a: '6',
+        b: '7',
+        c: '8',
+        d: '9',
+        correct: 'c'
+    },
+    {
+        question: 'What is the most used programming language in 2019 ?',
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'JavaScript',
+        correct: 'a'
+    },
+    {
+        question: 'Who was the president of US in 2012 ?',
+        a: 'George W. Bush',
+        b: 'Barack Obama',
+        c: 'Donald Trump',
+        d: 'Joe Biden',
+        correct: 'b'
+    },
+    {
+        question: 'Which of the following is used for concatenation in PHP ?',
+        a: '+ (plus)',
+        b: '* (Asterisk)',
+        c: 'append()',
+        d: '. (dot)',
+        correct: 'd'
+    },
+    {
+        question: 'What is a bug ?',
+        a: 'Computer Virus',
+        b: 'Error in Computer Configuration',
+        c: 'Error in a Programme',
+        d: 'None of these',
+        correct: 'c'
+    }
+]
